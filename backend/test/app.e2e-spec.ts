5b5b8d00956b5102bd3cb6f1ac74033c608730ec
YarikCompany2 <yaroslavromanenko456@gmail.com>
2026-03-26 23:19:53 +0200

    feat(backend): add e2e test

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,25 +1,125 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('Authentication & Companies (e2e)', () => {
   let app: INestApplication<App>;
 
-  beforeEach(async () => {
+  const testUser = {
+    email: `tester${Date.now()}@example.com`,
+    username: 'Tester',
+    password: 'password123',
+  };
+
+  let accessToken: string;
+  let refreshToken: string;
+  let userId: string;
+
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(new ValidationPipe({ transform: true }));
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/users/register (POST) - Success', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .post('/users/register')
+      .send(testUser)
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.id).toBeDefined();
+        userId = res.body.id;
+      });
+  });
+
+  it('/auth/login (POST) - Success', () => {
+    return request(app.getHttpServer())
+      .post('/auth/login')
+      .send({
+        email: testUser.email,
+        password: testUser.password,
+      })
       .expect(200)
-      .expect('Hello World!');
+      .expect((res) => {
+        expect(res.body.accessToken).toBeDefined();
+        expect(res.body.refreshToken).toBeDefined();
+        accessToken = res.body.accessToken;
+        refreshToken = res.body.refreshToken;
+      });
+
+      
+  });
+
+  it('/companies (POST) - Success with Token', () => {
+    return request(app.getHttpServer())
+      .post('/companies')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        name: 'E2E Test Studio',
+        description: 'Testing something',
+        type: 'developer'
+      })
+      .expect(201)
+      .expect((res) => {
+        expect(res.body.ownerId).toBe(userId);
+      });
+  });
+
+  it('/companies (POST) - Failure (No Token)', () => {
+    return request(app.getHttpServer())
+      .post('/companies')
+      .send({ name: 'Hack Studio' })
+      .expect(401);
+  });
+
+  it('/companies (POST) - Failure (User already owns a company)', () => {
+    return request(app.getHttpServer())
+      .post('/companies')
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        name: 'Second Empire',
+        type: 'publisher',
+      })
+      .expect(400)
+  });
+
+  let validRefreshToken: string;
+  let oldRefreshToken: string;
+
+  it('/auth/refresh (POST) - Success', async () => {
+    const res = await request(app.getHttpServer())
+      .post('/auth/refresh')
+      .send({
+        userId: userId,
+        refreshToken: refreshToken
+      })
+      .expect(200)
+    
+    expect(res.body.accessToken).toBeDefined();
+    expect(res.body.refreshToken).toBeDefined();
+    expect(res.body.refreshToken).not.toBe(refreshToken);
+
+    oldRefreshToken = refreshToken;
+    refreshToken = res.body.refreshToken;
+  });
+
+  it('/auth/refresh (POST) - Failure (Old Refresh Token)', async () => {
+    return request(app.getHttpServer())
+      .post('/auth/refresh')
+      .send({
+        userId,
+        refreshToken: oldRefreshToken,
+      })
+      .expect(401)
   });
 });
