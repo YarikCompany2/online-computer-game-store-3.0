48a9b88b70ef88989b3a6085253d62b74b251bc2
YarikCompany2 <yaroslavromanenko456@gmail.com>
2026-04-01 13:53:16 +0300

    feat: add bottom scroll bar for displaying screens in game-details

--- frontend/src/app/features/game-details/game-details.ts
+++ frontend/src/app/features/game-details/game-details.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject, signal } from '@angular/core';
+import { Component, OnDestroy, OnInit, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { GameService } from '../../core/services/game';
@@ -11,14 +11,20 @@ import { IGame, IMedia } from '../../core/interfaces/game.interface';
   templateUrl: './game-details.html',
   styleUrl: './game-details.scss'
 })
-export class GameDetailsComponent implements OnInit {
+export class GameDetailsComponent implements OnInit, OnDestroy {
   private route = inject(ActivatedRoute);
   private gameService = inject(GameService);
   
   game = signal<IGame | null>(null);
   isLoading = signal(true);
-
   selectedMedia = signal<IMedia | null>(null);
+
+  private autoSlideInterval: any;
+  private isAutoSlidingActive = signal(true);
+
+  screenshotsOnly = computed(() => 
+    this.game()?.media.filter(m => !m.isMain) || []
+  );
 
   ngOnInit() {
     const id = this.route.snapshot.paramMap.get('id');
@@ -26,15 +32,47 @@ export class GameDetailsComponent implements OnInit {
       this.gameService.getGameById(id).subscribe({
         next: (data) => {
           this.game.set(data);
-          const firstScreenshot = data.media.find(m => !m.isMain) || data.media[0];
-          this.selectedMedia.set(firstScreenshot);
+          
+          const firstScreenshot = data.media.find(m => !m.isMain);
+          this.selectedMedia.set(firstScreenshot || data.media[0]);
+          
           this.isLoading.set(false);
+          this.startAutoSlide();
         }
       });
     }
   }
+  
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  private startAutoSlide() {
+    this.autoSlideInterval = setInterval(() => {
+      if (this.isAutoSlidingActive()) {
+        this.nextMedia();
+      }
+    }, 4000);
+  }
+
+  private stopAutoSlide() {
+    if (this.autoSlideInterval) {
+      clearInterval(this.autoSlideInterval);
+    }
+  }
+
+  private nextMedia() {
+    const screens = this.screenshotsOnly();
+    if (screens.length <= 1) return;
+
+    const currentIndex = screens.indexOf(this.selectedMedia()!);
+    const nextIndex = (currentIndex + 1) % screens.length;
+    this.selectedMedia.set(screens[nextIndex]);
+  }
 
   selectMedia(media: IMedia) {
     this.selectedMedia.set(media);
+    this.isAutoSlidingActive.set(false);
+    this.stopAutoSlide();
   }
 }
